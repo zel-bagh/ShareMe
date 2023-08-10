106509d37f93fb2e5e90c4c6635b34cf2bd8d3d7
zel-bagh <zakaria.baghazaoui@gmail.com>
2023-08-10 22:41:11 +0100

    Login..1

--- shareme_frontend/src/containers/Login.jsx
+++ shareme_frontend/src/containers/Login.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { FcGoogle } from 'react-icons/fc';
-import logo from '../assets/logowhite.svg';
-import shareVideo from '../assets/sharevideo.svg';
+import logo from '../assets/logowhite.png';
+import shareVideo from '../assets/share.mp4';
 import { useNavigate } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { GoogleLogin, googleLogout } from '@react-oauth/google';
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+            src={shareVideo}
+            type="video/mp4"
+            loop
+            autoPlay
+            muted
+            className="w-full h-full object-cover"
+        />
+        
+      </div>
     </div>
   )
 }
